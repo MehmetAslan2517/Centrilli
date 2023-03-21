2e524a088d149413c4af898e64ef69be619e75ff
Mehmet Aslan <mehmetaslan2001@gmail.com>
2023-03-21 14:25:14 +0300

    hooks created

---
+++ src/test/java/com/cydeo/centrilli/step_definitions/Hooks.java
@@ -0,0 +1,23 @@
+package com.cydeo.centrilli.step_definitions;
+
+import com.cydeo.centrilli.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    //import from cucumber @After not junit @After
+    @After
+    public void teardownScenario(Scenario scenario){
+
+        if(scenario.isFailed()){
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png",scenario.getName());
+        }
+        Driver.closeDriver();
+        //System.out.println("===Closing browser using cucumber @After");
+        //System.out.println("===Scenario ended take screenshot if it is failed");
+    }
+}
