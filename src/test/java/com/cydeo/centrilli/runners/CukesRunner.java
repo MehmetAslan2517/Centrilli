e11e1d369587ef220fe6a5a8d3d3e90289c182ab
Mehmet Aslan <mehmetaslan2001@gmail.com>
2023-03-26 14:15:18 +0300

    json report added

--- src/test/java/com/cydeo/centrilli/runners/CukesRunner.java
+++ src/test/java/com/cydeo/centrilli/runners/CukesRunner.java
@@ -9,13 +9,14 @@ import org.junit.runner.RunWith;
         plugin = {
                 //"pretty",
                 "html:target/cucumber-report.html",
+                "json:target/cucumber.json",
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/centrilli/step_definitions",
         dryRun = false,
-        tags = "@wip",
+        tags = "@CENT10-330",
         publish = false
 )
 
